08b5ae8fb8611de86548597cedc4d1d202732529
RoberRamzy <berooramzy@gmail.com>
2024-10-28 15:56:11 +0300

    AdminRole was added

---
+++ src/AdminRole.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AdminRole {
+    private TrainerDatabase Database;
+    public AdminRole(){
+        Database=new TrainerDatabase("src/trainerDatabase");
+        Database.readFromFile();
+    }
+    public void addTrainer(String trainerId, String name, String email, String specialty, String phoneNumber){
+        Trainer trainer=new Trainer(trainerId,name,email,specialty,phoneNumber);
+        Database.insertRecord(trainer);
+    }
+    public ArrayList<Trainer> getListOfTrainers (){
+        ArrayList<Trainer> trainers;
+        ArrayList<DatabaseOBJ> data= Database.returnAllRecords();
+        trainers= (ArrayList<Trainer>) data.clone();
+        return trainers;
+    }
+    public void removeTrainer(String key){
+        Database.deleteRecord(key);
+    }
+    public void logout() throws IOException {
+        Database.saveToFile();
+    }
+
+}
