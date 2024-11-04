43b50b276f4d9884d469d480b453c53475f76a6b
RoberRamzy <berooramzy@gmail.com>
2024-11-05 00:09:59 +0200

    TrainerRole fixed

--- src/frontend/TrainerRoleWindow.java
+++ src/frontend/TrainerRoleWindow.java
@@ -4,9 +4,7 @@ import backend.TrainerRole;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
+
 
 public class TrainerRoleWindow
  extends JFrame{
