2babce602289a38c119cee43acf5564e69d504d8
minaemil10 <minaemile3@gmail.com>
2024-11-04 17:49:21 +0200

    AddMember form added

---
+++ src/frontend/AddMember.java
@@ -0,0 +1,20 @@
+package frontend;
+
+import javax.swing.*;
+
+public class AddMember {
+    private JButton AddButton;
+    private JTextField TextId;
+    private JLabel Id;
+    private JTextField TextName;
+    private JTextField TextEmail;
+    private JTextField TextPhoneNumber;
+    private JLabel Name;
+    private JLabel Email;
+    private JLabel Speciality;
+    private JLabel PhoneNumber;
+    private JTextField TextMembershipType;
+    private JPanel container5;
+    private JLabel Status;
+    private JTextField TextStatus;
+}
