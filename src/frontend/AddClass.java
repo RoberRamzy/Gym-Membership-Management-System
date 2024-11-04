5cb53c8a56c57d5021ea713ab67d7c18eef096bf
minaemil10 <minaemile3@gmail.com>
2024-11-04 18:19:36 +0200

    added add class

---
+++ src/frontend/AddClass.java
@@ -0,0 +1,19 @@
+package frontend;
+
+import javax.swing.*;
+
+public class AddClass {
+    private JButton AddButton;
+    private JTextField TextClassId;
+    private JLabel ClassId;
+    private JTextField TextClassName;
+    private JTextField TextTrainerId;
+    private JLabel ClassName;
+    private JLabel TrainerId;
+    private JLabel Duration;
+    private JLabel PhoneNumber;
+    private JTextField TextDuration;
+    private JLabel MaxParticipants;
+    private JTextField TextMaxParticipants;
+    private JPanel Container6;
+}
