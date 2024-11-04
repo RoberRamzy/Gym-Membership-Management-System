f1fc1528a58a2c2b3bd0de9ab935c0a6140529f4
minaemil10 <minaemile3@gmail.com>
2024-11-04 19:41:29 +0200

    registermember added

---
+++ src/frontend/RegisterMember.java
@@ -0,0 +1,13 @@
+package frontend;
+
+import javax.swing.*;
+
+public class RegisterMember {
+    private JButton RegisterButton;
+    private JTextField TextMemberId;
+    private JLabel MemberId;
+    private JPanel Container9;
+    private JLabel ClassId;
+    private JTextField TextClassId;
+    private JLabel RegistrationDate;
+}
