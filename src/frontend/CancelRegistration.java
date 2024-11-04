1a3fc2cf73813ffa23f23b4876b6341bd9b5cb67
minaemil10 <minaemile3@gmail.com>
2024-11-04 20:15:52 +0200

    Cancel Registration Panel added

---
+++ src/frontend/CancelRegistration.java
@@ -0,0 +1,12 @@
+package frontend;
+
+import javax.swing.*;
+
+public class CancelRegistration {
+    private JLabel MemberId;
+    private JTextField TextMemberId;
+    private JButton CancelButton;
+    private JPanel Container10;
+    private JLabel ClassId;
+    private JTextField TextClassId;
+}
