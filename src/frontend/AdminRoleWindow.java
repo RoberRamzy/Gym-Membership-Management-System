2b542a49dcbbb1159a2d45b26285eed369ec1169
RoberRamzy <berooramzy@gmail.com>
2024-11-04 17:19:44 +0200

    AdminRoleLogout Logic

--- src/frontend/AdminRoleWindow.java
+++ src/frontend/AdminRoleWindow.java
@@ -1,6 +1,11 @@
 package frontend;
 
+import backend.AdminRole;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class AdminRoleWindow {
     private JButton AddTrainer;
@@ -8,4 +13,20 @@ public class AdminRoleWindow {
     private JButton ViewTrainers;
     private JButton Logout;
     private JPanel Container3;
+    private AdminRole Admin;
+    public AdminRoleWindow() {
+        Admin=new AdminRole();
+        /*Log out*/
+        Logout.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Admin.logout();
+                    JOptionPane.showMessageDialog(Container3,"Log out successfully","Success",JOptionPane.INFORMATION_MESSAGE);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+    }
 }
