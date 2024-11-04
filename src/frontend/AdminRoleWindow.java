b99a59bda5a6f993b1afc6dd9fff04d30b5edf1c
minaemil10 <minaemile3@gmail.com>
2024-11-04 15:44:21 +0200

    Added main menu and admin role window in the frontend

---
+++ src/frontend/AdminRoleWindow.java
@@ -0,0 +1,11 @@
+package frontend;
+
+import javax.swing.*;
+
+public class AdminRoleWindow {
+    private JButton AddTrainer;
+    private JButton RemoveTrainer;
+    private JButton ViewTrainers;
+    private JButton Logout;
+    private JPanel Container3;
+}
--- src/frontend/MainWindow.java
+++ src/frontend/MainWindow.java
@@ -1,4 +1,12 @@
 package frontend;
 
+import javax.swing.*;
+
 public class MainWindow {
+    private JButton AdminRole;
+    private JPanel container1;
+    private JButton TrainerRole;
+
+
+
 }
