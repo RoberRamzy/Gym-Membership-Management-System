69c65a8e792eae48da122df3c6aa3a9b0bd4284e
minaemil10 <minaemile3@gmail.com>
2024-11-04 17:15:17 +0200

    Trainer Form made

---
+++ src/frontend/TrainerWindow.java
@@ -0,0 +1,18 @@
+package frontend;
+
+import javax.swing.*;
+
+public class TrainerWindow {
+    private JButton AddButton;
+    private JTextField TextId;
+    private JLabel Id;
+    private JPanel Container4;
+    private JLabel Name;
+    private JLabel Email;
+    private JLabel Speciality;
+    private JLabel PhoneNumber;
+    private JTextField TextName;
+    private JTextField TextEmail;
+    private JTextField TextSpeciality;
+    private JTextField TextPhoneNumber;
+}
