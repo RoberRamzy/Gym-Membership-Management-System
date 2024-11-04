9db21cf6ddb05776ffe65c1eb6379f242014ca54
minaemil10 <minaemile3@gmail.com>
2024-11-04 17:32:00 +0200

    DeleteTrainer made

---
+++ src/frontend/DeleteTrainer.java
@@ -0,0 +1,9 @@
+package frontend;
+
+import javax.swing.*;
+
+public class DeleteTrainer {
+    private JButton RemoveButton;
+    private JTextField TextTrainerId;
+    private JLabel TrainerId;
+}
